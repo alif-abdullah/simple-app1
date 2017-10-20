cd3620b4f5438da982adf9b444cba83136351b49
alif abdullah <alifabdullah26709@gmail.com>
2017-10-20 18:20:01 +0600

    make it better

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require ("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get ("/", function (req,res){
+    res.send ("Welcome");
+});
+app.listen (port);
